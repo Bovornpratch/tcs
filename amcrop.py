818f44538acec3b838a3c30a0fd66b6f6bcc2067
Bovornpratch <bovornpratch.v@gmail.com>
2017-09-22 21:53:51 +0700

    filter zero

--- amcrop.py
+++ amcrop.py
@@ -12,6 +12,9 @@ def amcrop(time,am,cut):
     cut = total observation time
     '''
     time,am = numpy.array(time),numpy.array(am) 
+    filt_z = numpy.where(am >= 1.)
+    time,am = time[filt_z],am[filt_z]
+
     cen_in = numpy.where(numpy.amin(am) == am)
     basin_t,basin_am = time[cen_in][0],am[cen_in][0]
     lim = datetime.timedelta(minutes=cut/2.)
@@ -26,9 +29,16 @@ def main():
     date_list = [base + datetime.timedelta(minutes=x) for x in range(0,600)]
     zenith = numpy.absolute(numpy.linspace(-90,90,600))
 
+    extend = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    alt_ext =[-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10] 
+    date_list_ext = [date_list[len(date_list)-1] + datetime.timedelta(minutes=x) for x in range(0,len(extend))]
+    
     alt = numpy.absolute(zenith - 90.)
     airmass = 1./numpy.cos(numpy.radians(zenith))
 
+    date_list = numpy.append(date_list,date_list_ext)
+    alt = numpy.append(alt,alt_ext)
+    airmass = numpy.append(airmass,extend)
 
     plt.plot(date_list[1:],alt[1:],color='r',label='altitude')
     plt.legend()
